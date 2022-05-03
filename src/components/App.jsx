c6b8e4db94d9594023c64aef89499adec004b273
Tanesh <arvesh1701gmail.comarvesh1701gmail.com>
2022-05-03 11:48:34 +0400

    added functionalities

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
+import { useEffect } from "react";
 
 function App() {
+  const [inputText, setInput] = useState('');
+  const [notes, setNotes] = useState(() => {
+    const savednotes = localStorage.getItem("notes");
+    if (savednotes) {
+      return JSON.parse(savednotes);
+    } else {
+      return [];
+    }
+  });
+
+  useEffect(() => {
+    localStorage.setItem('notes', JSON.stringify(notes));
+  }, [notes]);
+
+  function handleChange(event) {
+    const newValue = event.target.value;
+    setInput(newValue);
+  }
+
+  function addNote() {
+    setNotes((prevValues) => {
+      return [...prevValues, inputText]
+    });
+    setInput("");
+  }
+
   return (
     <div className="container">
       <div className="heading">
         <h1>To-Do List</h1>
       </div>
       <div className="form">
-        <input type="text" />
-        <button>
+        <input type="text" value={inputText} onChange = {handleChange}/>
+        <button onClick={addNote}>
           <span>Add</span>
         </button>
       </div>
       <div>
         <ul>
-          <li>An item</li>
+          {notes.map((note) => <li>{note}</li>)}
         </ul>
       </div>
     </div>
